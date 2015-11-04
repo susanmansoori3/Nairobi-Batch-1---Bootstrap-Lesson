cc67d52b0fa134882188a5616027379bc0c37a4c
dmuthuri <muthuridennis@gmail.com>
2015-11-04 13:14:56 +0300

    Adds form submission using ajax && a bootstrap lesson plan

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # Lesson on bootstrap
+1. Integrating bootstrap.
+2. Understanding the grid system.
+3. Using the bootstrap grid system to layout a wireframe.
+4. Adding bootstrap components to your wireframe.
+5. Working with Ajax.
+
 
 ## Project roadmap
 - Add awesome pitch
--- js/main.js
+++ js/main.js
@@ -5,5 +5,25 @@ $(document).ready(function() {
 					.mouseleave(function(){
 						$(this).removeClass('animated shake');
 					});
+	// Some people represent the event parameter as 'e'
+	// this is a naming shortcut 
+	$('#contact-form').on('submit', function(e){
+		// Place event handling logic here
+		url = '/';
+		data = $(this).serialize();
+
+		$.post(url, data)
+		 .done(function() {
+				// if the data was submitted successfully: alert('You are logged in');
+				alert('You are logged in');		 	
+		 }).fail(function() {
+				// if the data was submitted and there was an error: alert('Ooops. Something went wrong');
+				alert('Ooops. Something went wrong'); 	
+		 });
+
+
+		// Prevents the default behaviour of the form
+		e.preventDefault();
+	});
 
 });
